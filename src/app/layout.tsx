14201b1bfca6c151c6e5ab8e3feeacc9ed37363f
Santiago Espinal <santiagoespinal333@gmail.com>
2025-09-20 18:38:26 -0500

    fix :correccion responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <HeroUIProvider>
             <ToastProvider
               placement="top-right"
-              toastOffset={110}
+              toastOffset={100}
               maxVisibleToasts={10}
             />
           <div className="container mx-auto overflow-x-hidden px-5 sm:px-10 md:px-10 lg:px-10 xl:px-16  max-w-[1440px] min-h-screen h-full">
