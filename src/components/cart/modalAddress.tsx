7ed116a304dae7d3346f4114b4128db0297f7e62
Santiago Espinal <santiagoespinal333@gmail.com>
2025-10-23 21:19:16 -0500

    fix: buttons orientation

--- src/components/cart/modalAddress.tsx
+++ src/components/cart/modalAddress.tsx
@@ -262,7 +262,7 @@ export const ModalAddress = ({
               </div>
             </div>
 
-            <div className="flex px-[20px] w-full gap-7 pb-[24px] justify-center mt-[16px] lg:pl-[32px] lg:pb-[32px] lg:mt-[32px]">
+            <div className="flex px-[20px] w-full gap-7 pb-[24px] justify-center lg:justify-between mt-[16px] lg:px-[45px] lg:mt-[32px]">
               <Button
                 type="button"
                 className="text-neutral-black-80 bg-accent-yellow-40 hover:bg-accent-yellow-60 active:bg-accent-yellow-60 rounded-[30px] flex items-center gap-2 text-[16px] w-[151px] h-[40px] lg:w-[200px] lg:h-[48px]"
