ce8602d1296ff21c0722f1544e91f0cbc4759b44
PedroPab <pmontesarroyave@gmail.com>
2025-11-18 01:13:51 -0500

    ft: agregar script de seguimiento de usuarios en el layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <meta name="apple-mobile-web-app-title" content="Domi Burguer" /></head>
+      <script defer src="https://umami.toxolinko.site/script.js" data-website-id="eae720e3-134b-4d76-a2fd-1d74b32bbcca"></script>
       <body
         className={montserrat.variable}
       >
