2528f755142b7b74605cea362303b8a6b576a65a
PedroPab <pmontesarroyave@gmail.com>
2026-01-03 15:22:10 -0500

    feat: Añadir página de verificación de teléfono y redirección para usuarios no autenticados

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from "react";
 import { CheckoutFormProvider } from "@/contexts/CheckoutFormContext";
+import { useRouter } from "next/navigation";
 
 import { useCartSubmit } from "@/hooks/cart/useCartSubmit";
 
@@ -10,13 +11,32 @@ import { LoadingOverlay } from "@/components/cart/loadingOverlay";
 import { CheckoutForm } from "@/components/cart/CheckoutForm";
 import { CartSummary } from "@/components/cart/CartSummary";
 import { CartModals } from "@/components/cart/CartModals";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function Cart() {
 
+  const router = useRouter();
   const { handleSubmitWithValidation, isSubmitting, error } = useCartSubmit();
   useEffect(() => {
     console.log(error)
   }, [error])
+
+  //revisamos si el cliente tiene un numero verificado en su cuenta de google si esta autenticado
+  const { user } = useAuth();
+  useEffect(() => {
+    if (user) {
+      console.log("User is authenticated:", user);
+      if (user.phoneNumber) {
+        console.log("User has a verified phone number:", user.phoneNumber);
+      } else {
+        console.log("User does not have a verified phone number.");
+        //lo mandamos a verificar el telefono
+        router.push("/verify-phone");
+      }
+    } else {
+      console.log("No authenticated user.");
+    }
+  }, [user]);
 
   return (
     <CheckoutFormProvider>
